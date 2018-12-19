fdf988a9fb194c24924746254f74a3148b46a360
Ben Parsons <ben@bpulse.co.uk>
2018-12-19 17:41:23 +0000

    working version

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+var synth = window.speechSynthesis;
+var voices = [];
+window.utterances = [];
+
+function configureVoices() {
+  populateVoices();
+  window.speechSynthesis.onvoiceschanged = function() {
+    console.log("onvoiceschanged");
+    populateVoices();
+  }
+}
+configureVoices();
+function populateVoices() {
+  console.log("populateVoices");
+  voices = [];
+  for (var voice_index in window.speechSynthesis.getVoices()) {
+    if (window.speechSynthesis.getVoices()[voice_index].lang.indexOf("en") === 0) {
+      voices.push(window.speechSynthesis.getVoices()[voice_index]);
+    }
+  }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +41,8 @@ class App extends Component {
       roomEntry: '#matrix:matrix.org',
       startEventId: '$15451748443784682ewPHy:matrix.org',
       messageCount: 20,
-      roomId: ''
+      roomId: '',
+      events: []
     }
   }
 
@@ -48,20 +77,11 @@ class App extends Component {
         </tr>
         </tbody>
         </table>
-        
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ScriptGrid
+        parts={this.state.parts}
+        currentLine={this.state.line}
+        script={this.state.script}
+        nextLine={this.nextLine.bind(this)} />
       </div>
     );
   }
@@ -79,13 +99,118 @@ class App extends Component {
       this.setState({roomId: this.state.roomEntry});
     }
 
-    const url = `https://matrix.org/_matrix/client/r0/rooms/${encodeURIComponent(this.state.roomId)}/context/${encodeURIComponent(this.state.startEventId)}?limit=${this.state.messageCount * 2 - 1}&access_token=${access_token}`;
+    const url = `https://matrix.org/_matrix/client/r0/rooms/${encodeURIComponent(this.state.roomId)}/context/${encodeURIComponent(this.state.startEventId)}?limit=100&access_token=${access_token}`;
 
     axios.get(url)
     .then(res => {
+      this.setState({events: [res.data.event].concat(res.data.events_after.filter(e => e.content.body))});
       console.log(res.data);
+      this.nextLine();
     })
 
+  }
+
+  nextLine() {
+    var line = this.state.line;
+    if (! this.state.events[line]) return;
+    var newPart = this.state.events[line].sender;
+    if (! this.state.parts.find(p =>{return p.name === newPart;})) {
+      this.setState({
+        parts: this.state.parts.concat([{
+          name: newPart,
+          voice: voices[getRandomInt(0, voices.length)]
+        }])
+      })
+    }
+    this.setState({
+      script: this.state.script.concat(this.state.events[line]),
+      line: this.state.line + 1,
+      nextText: "Continue"
+    });
+  }
+}
+
+class ScriptGrid extends Component {
+  render() {
+    const parts = this.props.parts.map((part, i) => {
+      const lines = this.props.script.map((line, lineNumber) => { 
+        line.lineNumber = lineNumber;
+        return line; 
+      }).filter(l => l.sender === part.name);
+
+      return (
+        <Part key={"part" + i} part={part}
+        lines={lines}
+        currentLine={this.props.currentLine}
+        nextLine={this.props.nextLine}
+        />
+      );
+    });
+    return (
+      <div className="container-outer">
+        <div className="container-inner">
+          <div>{parts}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Part extends Component {
+  render() {
+    const lines = this.props.lines.map((line, index) => {
+      return (
+        <Line
+        key={"line"+index}
+        lineText={line.content.body.split('\n').filter(l => l[0] !== '>' && l !== '').join('\n')}
+        lineNumber={line.lineNumber}
+        currentLine={this.props.currentLine}
+        part={this.props.part}
+        nextLine={this.props.nextLine} />
+      );
+    });
+    var age = this.props.currentLine - Math.max(...this.props.lines.map(l => {return l.lineNumber}));
+    const divStyle = {
+      display: age > 5 ? 'none' : 'initial'
+    };
+    return (
+      <div 
+      style={divStyle}
+      className="part-column">
+        <div className="part-name">{this.props.part.name}</div>
+        {lines}
+      </div>
+    )
+  }
+}
+
+class Line extends Component {
+  constructor(props) {
+    super(props);
+    var utterance = new SpeechSynthesisUtterance();
+    utterance.addEventListener('end', function () {
+      console.log("stopped: " + utterance.text);
+    });
+    var nextLine = this.props.nextLine;
+    utterance.onend = function(a) {
+      console.log("ended: " + utterance.text);
+      nextLine();
+    };
+    utterance.text = this.props.lineText;
+    utterance.voice = this.props.part.voice;
+    window.utterances.push(utterance); // makes utterance.onend more reliable in Chrome. it's true!
+    synth.speak(utterance);
+    console.log(utterance);
+  }
+
+  render() {
+    const divStyle = {
+      top: 30 + (this.props.lineNumber%10)*50 + "px",
+      opacity: this.props.lineNumber + 1 === this.props.currentLine ? 1 :0.1
+    };
+    return(
+      <div ref={"line"+this.props.lineNumber} style={divStyle}>{this.props.lineText}</div>
+    );
   }
 }
 
